b657a8385b59d66f7d3889161304c3842238d496
Anton Smorodskiy <asmorodskiy@lohika.com>
2012-02-21 23:51:55 +0200

    After reading Hibernate in Action + Spring in Action

---
+++ src/com/antonbanking/dao/MyTransactionsORM.java
@@ -0,0 +1,10 @@
+package com.antonbanking.dao;
+
+import org.hibernate.annotations.Entity;
+import org.hibernate.annotations.Table;
+
+@Entity
+@Table( appliesTo = "")
+public class MyTransactionsORM {
+
+}
